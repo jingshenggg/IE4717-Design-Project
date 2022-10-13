b74099bbd44efda4fb0035dbc701ba43ee3bfdec
jing sheng <teyjingsheng@gmail.com>
2022-10-13 22:14:59 +0800

    User registation and user login page w/o css yet

---
+++ shop_database.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `user_form` (
+  `id` int(100) NOT NULL AUTO_INCREMENT,
+  `name` varchar(40) NOT NULL,
+  `email` varchar(40) NOT NULL,
+  `password` varchar(40) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4
