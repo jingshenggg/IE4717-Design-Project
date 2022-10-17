65f99e46f30dc2b444f72b0f9310f90d5cb5c3e4
jing sheng <teyjingsheng@gmail.com>
2022-10-17 15:39:55 +0800

    changing index.html to index.php

--- login.php
+++ login.php
@@ -13,7 +13,7 @@ if(isset($_POST['submit'])){
    if(mysqli_num_rows($select) > 0){
       $row = mysqli_fetch_assoc($select);
       $_SESSION['user_id'] = $row['id'];
-      header('location:index.html');
+      header('location:index.php');
    }else{
       $message[] = 'incorrect password or email!';
    }
